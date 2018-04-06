8c4917d015204578405c59a9c5e2aa768df87a43
GeorgeKeith <george@techfeathers.com>
2018-04-06 01:02:52 -0500

    Cleaned up code

--- goTraining/goFactorial/main.go
+++ goTraining/goFactorial/main.go
@@ -21,11 +21,9 @@ func factorial(n int) int32 {
 	if (n < m) {
 		m = n
 	}
-//	fmt.Println("m = ", m)
 	term := make(chan int)
 	done := make(chan bool)
 	stride := n / m
-//	fmt.Println("stride = ", stride)
 	go func() {
 		for i := 0; i < m; i++ {
 			<- done
@@ -33,32 +31,25 @@ func factorial(n int) int32 {
 		close(term)
 	}()
 	extra := n % m
-//	fmt.Println("extra = ", extra)
 	h := 0
 	for i := 0; i < m; i++ {
 		l := h + 1
-//		fmt.Println("l = ", l)
 		h = h + stride
 		if extra > 0 {
 			h++
 			extra--
 		}
-//		fmt.Println("h = ", h)
 		go func(low int, high int){
 			var result int32 = 1
 			for i := low; i <= high; i++ {
-//				fmt.Printf("%v-%v: i = %v\n", low, high, i)
 				result *= int32(i)
-//				fmt.Println("result = ", result)
 			}
-//			fmt.Printf("%v-%v: result = %v\n", low, high, result)
 			term <- int(result)
 			done <- true
 		}(l, h)
 	}
 	var result int32 = 1
 	for x := range term {
-//		fmt.Println("x = ", x)
 		result *= int32(x)
 	}
 	return result
